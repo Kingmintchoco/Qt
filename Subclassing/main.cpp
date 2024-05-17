d0d9ede76bbb56b0b7f4cf24ee082b2717da3e64
Kingmintchoco <jrkr7@naver.com>
2024-05-18 00:02:12 +0900

    self Signal
    init self signal

--- Subclassing/main.cpp
+++ Subclassing/main.cpp
@@ -6,6 +6,9 @@ int main(int argc, char *argv[]){
     QApplication a(argc, argv);
 
     MyDialog* dialog = new MyDialog;
+
+    QObject::connect(dialog, SIGNAL(onKeywordMatched(const QString&)), &a, SLOT(quit()));
+    
     dialog->show();
 
     return a.exec();
--- Subclassing/mydialog.cpp
+++ Subclassing/mydialog.cpp
@@ -1,8 +1,18 @@
 #include "mydialog.h"
 #include <QMessageBox>
 #include <QCloseEvent>
+#include <QLineEdit>
+#include <QVBoxLayout>
 
-MyDialog::MyDialog(QWidget* parent):QDialog(parent){    
+MyDialog::MyDialog(QWidget* parent):QDialog(parent){ 
+    lineEdit = new QLineEdit;
+
+    connect(lineEdit, SIGNAL(textChanged(const QString&)), this, SLOT(myTextChanged(const QString&)));
+
+    QVBoxLayout* layout = new QVBoxLayout;
+    layout->addWidget(lineEdit);
+
+    setLayout(layout);   
 }
 
 void MyDialog::closeEvent(QCloseEvent *event){
@@ -13,4 +23,10 @@ void MyDialog::closeEvent(QCloseEvent *event){
         event->accept();            // event를 허용
     else
         event->ignore();            // event를 거절
+}
+
+void MyDialog::myTextChanged(const QString& text){
+    if(text == "s2hyun"){
+        emit onKeywordMatched(text);
+    }
 }
